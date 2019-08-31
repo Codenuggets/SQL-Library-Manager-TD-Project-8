8829bb5785a7720655f023a50b4e7f3ea26940e6
Codenuggets <wd14link@gmail.com>
2019-08-30 21:34:54 -0400

    Added Search functionality and error routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const db = require ('./db');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const { Book } = db.models;
+
 
 const app = express();
 
@@ -10,14 +13,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/static', express.static('public'));
 
-(async () => {
-  try {
-    await db.sequelize.authenticate();
-    console.log('Success!');
-  } catch (error) {
-    console.error('Error connecting, ', error);
-  }
-})();
+// async function search() {
+//   const searchInput = document.querySelector('.search');
+//   const results = await Book.findAll({
+//     where: {
+//       title: {
+//         $iLike: `%${searchInput.value}%`,
+//       },
+//       $or: [
+//         {
+//           author:
+//           {
+//             $iLike: `%${searchInput.value}%`,
+//           }
+//         },
+//         {
+//           genre:
+//           {
+//             $iLike: `%${searchInput.value}%`,
+//           }
+//         }
+//       ]
+//     }
+//   });
+//   console.log("hi!");
+// }
 
 app.get('/', (req, res) => {
   res.redirect('/books');
@@ -30,6 +50,46 @@ app.get('/books', async (req, res) => {
     res.render('index', { books });
   } catch (err) {
     console.error('Error grabbing books ', err);
+  }
+});
+
+app.post('/books', async(req, res) => {
+  try {
+    const splitYear = req.body.searchInput.split('');
+    const books = await Book.findAll({
+      where: {
+        [Op.or]: [
+          {
+            title:
+            {
+              [Op.like]: `%${req.body.searchInput}%`
+            },
+          },
+          {
+            author:
+            {
+              [Op.like]: `%${req.body.searchInput}%`
+            },
+          },
+          {
+            genre:
+            {
+              [Op.like]: `%${req.body.searchInput}%`
+            },
+          },
+          {
+            year:
+            {
+              [Op.contains]: `${splitYear.map(Number)}`
+            }
+          },
+        ]
+      }
+    });
+    res.render('index', { books });
+    res.end();
+  } catch (err) {
+    console.error('Error searching for books ', err);
   }
 });
 
@@ -59,10 +119,32 @@ app.post('/books/new', async (req, res) => {
 app.get('/books/:id', async (req, res) => {
   try {
     const selectedBook = await Book.findByPk(req.params.id);
+    if(selectedBook === null) {
+      const err = new Error("Sever Error");
+      err.status = 500;
+      res.render('error', { err });
+    }
     const book = selectedBook.toJSON();
     res.render('update-book', { book });
   } catch (err) {
     console.error("Error grabbing book ", err);
+  }
+});
+
+app.post('/books/:id', async (req, res) => {
+  try {
+    const selectedBook = await Book.findByPk(req.params.id);
+    await selectedBook.update({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      year: req.body.year
+    });
+    const book = selectedBook.toJSON();
+    res.render('update-book', { book, success: "Successfully updated book!"});
+    res.end();
+  } catch (err) {
+    console.error("Error updating book ", err);
   }
 });
 
@@ -76,6 +158,18 @@ app.post('/books/:id/delete', async (req, res) => {
   } catch (err) {
     console.error("Error deleting book ", err);
   }
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.err = err;
+  res.status(err.status);
+  res.render('page-not-found');
 });
 
 const port = process.env.port || 3000;
